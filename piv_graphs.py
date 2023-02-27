5a6ce564f486852ece59ab01f6384f9b6228c304
Diogo Fernandes <diogo@diogos.cf>
2023-02-27 11:46:25 +0100

    PIV

--- piv_graphs.py
+++ piv_graphs.py
@@ -1,9 +1,22 @@
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from matplotlib.colors import Normalize
 import numpy as np
 
 from load_data import piv_data
 
+xcount, ycount = 395, 57
+ylim = 57
+minl = (57 - ylim)*395
+step = 5 # Resolution
 fig_piv, ax_piv = plt.subplots()
-ax_piv.quiver(piv_data[:, 0], piv_data[:, 1], piv_data[:, 2], piv_data[:, 3], pivot="mid", scale=100, scale_units="xy", width=0.002, headwidth=3, headlength=4, headaxislength=3)
+X, Y = piv_data[minl::step, 0], piv_data[minl::step, 1]
+U, V = piv_data[minl::step, 2], piv_data[minl::step, 3]
+
+colors = np.linalg.norm(np.column_stack((U, V)), axis=1)
+norm = Normalize()
+norm.autoscale(colors)
+
+ax_piv.quiver(X, Y, U, V, color=cm.inferno(norm(colors)), pivot="mid", scale=100, scale_units="xy", width=0.001, headwidth=3, headlength=4, headaxislength=3)
 
 plt.show()
