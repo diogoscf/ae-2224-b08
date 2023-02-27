a6ba4dabaa98eaec6894da84c3d0a48c447d743d
Diogo Fernandes <diogo@diogos.cf>
2023-02-27 12:30:03 +0100

    updated PIV graph

--- piv_graphs.py
+++ piv_graphs.py
@@ -8,15 +8,19 @@ from load_data import piv_data
 xcount, ycount = 395, 57
 ylim = 57
 minl = (57 - ylim)*395
-step = 5 # Resolution
+step = 20 # Resolution
 fig_piv, ax_piv = plt.subplots()
 X, Y = piv_data[minl::step, 0], piv_data[minl::step, 1]
 U, V = piv_data[minl::step, 2], piv_data[minl::step, 3]
 
 colors = np.linalg.norm(np.column_stack((U, V)), axis=1)
+colors /= np.max(colors)
 norm = Normalize()
 norm.autoscale(colors)
 
-ax_piv.quiver(X, Y, U, V, color=cm.inferno(norm(colors)), pivot="mid", scale=100, scale_units="xy", width=0.001, headwidth=3, headlength=4, headaxislength=3)
+ax_piv.quiver(X, Y, U, V, colors, pivot="mid", scale=100, scale_units="xy", width=0.001, headwidth=3, headlength=4, headaxislength=3)
+# ax_piv.set_title("PIV data for AoA 2°, Re = 200 000")
+ax_piv.set_xlabel("x/c [-]")
+ax_piv.set_ylabel("y/c [-]")
 
 plt.show()
